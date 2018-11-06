c62376f21444a1a8adb8204969c4a57c92837ba0
Odemakinde Elisha Jesutofunmi <elishatofunmi@gmail.com>
2018-11-06 05:58:50 +0000

    Add files via upload

---
+++ kaggle project 1/R files/R_on_machine_learning_intro.py
@@ -0,0 +1,10 @@
+# importing pacakges and libararies
+
+import numpy as np
+import pandas as pd
+import scipy as sp
+import matplotlib.pyplot as plt
+import sklearn as sk
+
+# importing the dataset to be analyszed
+pd.read_csv(")
